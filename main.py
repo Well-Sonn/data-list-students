bbd86cc09f88d7ad5ed37ae72db314497bca2225
Well-Sonn <wellerson819@gmail.com>
2024-11-08 22:35:23 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/')
+def index():
+    return render_template('html/acesso.html')  
+if __name__ == '__main__':
+    app.run(debug=True)
